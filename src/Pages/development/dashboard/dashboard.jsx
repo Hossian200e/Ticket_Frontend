93b5e2a06822346be640644901b6b8363fcb6daf
Hossian <hossain200e@gmail.com>
2026-04-07 13:02:55 +0600

    add

---
+++ src/Pages/development/dashboard/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DevelopmentDashboard = () => {
+  return (
+    <div>
+      <h2>Development Dashboard</h2>
+    </div>
+  );
+};
+
+export default DevelopmentDashboard;
